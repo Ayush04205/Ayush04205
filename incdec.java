d32c4e6975259f7ea5303c502d0146d6d9c3f6be
Ayush04205 <patel.ayush04062005@gmail.com>
2024-08-05 14:05:16 +0530

    My first git commit

---
+++ incdec.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public static void main(String[] arg)
+{
+    Scanner sc = new Scanner (System.in);
+
+    int a = 2;
+    System.out.println(a++);
+    System.out.println(a);
+    System.out.println(++a);
+    System.out.println(a);
+
+    sc.close();
+}
