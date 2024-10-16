efbe671f84ffbaa7431b7051251b44bcfd84cf16
Ayush04205 <patel.ayush04062005@gmail.com>
2024-10-16 12:23:32 +0530

    Add files via upload

---
+++ java project/LibraryManagementSystem.java
@@ -0,0 +1,11 @@
+import Project.Library;
+import Project.User;
+
+public class LibraryManagementSystem {
+    public static void main(String[] args) {
+        Library library = new Library();
+        User user = new User(library);
+        user.start();
+    }
+}
+1
